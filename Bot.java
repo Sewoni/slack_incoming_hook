cda2673f657c2635f65f9e45774d347f776b622c
Sewoni <sewonkim1008@gmail.com>
2025-02-20 17:23:58 +0900

    modify error

--- Bot.java
+++ Bot.java
@@ -1,22 +1,25 @@
-import java.net.*:
-import java.net.http.*:
-import java.time.*:
-import java.util.*:
+import java.net.*;
+import java.net.http.*;
+import java.time.*;
+import java.util.*;
 
 public class Bot {
-    public static void main(String[] args) {
-        String webhookUrl=System.getenv("SLACK_WEBHOOK_URL");
+    public static void main(String[] args) { // 진입부분
+        // 이게 있어야 이 클래스를 실행했을 때 작동을 함
+        // 웹훅을 만들 거임 -> URL 필요함
+        // 환경변수로 받아올 것임 -> yml 파일에서 전달하게
+        String webhookUrl = System.getenv("SLACK_WEBHOOK_URL");
 
-        HttpClient client= HttpClient.newHttpClient();
-
-        HttpRequest request=HttpRequest.newBuilder();
+        // Java 11 -> fetch
+        HttpClient client = HttpClient.newHttpClient();
+        // 요청을 얹힐 거다
+        HttpRequest request = HttpRequest.newBuilder()
             .uri(URI.create(webhookUrl))
             .header("Content-Type", "application/json")
             .POST(HttpRequest.BodyPublishers.ofString("{\"text\":\"테스트 메시지\"}")) 
             .build();
-
-
-            try {
+        
+        try {
             HttpResponse<String> response = client.send(
                 request, HttpResponse.BodyHandlers.ofString()
             );
@@ -27,4 +30,3 @@ public class Bot {
         }
     }
 }
-
